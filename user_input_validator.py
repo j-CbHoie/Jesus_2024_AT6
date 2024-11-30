b3bf7ce75e22e82fc4cd028571de586e93a594ef
Jesus Magtibay <jesmag00899@stud.noroff.no>
2024-11-30 13:48:24 +0100

    Commit 1: Added display_validation_message method and extra_info.txt

--- user_input_validator.py
+++ user_input_validator.py
@@ -1,8 +1,16 @@
 class UserInputValidator:
     def validate_positive_integers(self, input_list):
-        """Validate and return a list of positive integers from the input list."""
+        """
+        Validate and return a list of positive integers from the input list.
+        """
         valid_numbers = []
         for item in input_list:
             if item.isdigit() and int(item) > 0:
                 valid_numbers.append(int(item))
         return valid_numbers
+
+    def display_validation_message(self):
+        """
+        Display a message after validating the list.
+        """
+        print("Validation complete. The list has been processed.")
